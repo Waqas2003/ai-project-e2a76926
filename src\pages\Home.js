ce311c35c3bb4953cf98d6761b7fc1875b919ce8
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 00:57:42 +0500

    Add src\pages\Home.js

---
+++ src\pages\Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Pakistan Cricket Team</h1>
+      <p>Get the latest news, schedule, and player information here.</p>
+    </div>
+  );
+}
+
+export default Home;
